2c2579ad87f1ef85162376fa3c3bc440c546d232
Ashik2630 <ashik426370@gmail.com>
2026-01-19 17:28:51 +0600

    complet banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   theme: {
     extend: {
+      backgroundImage:{
+        bannerImage:"url('./assets/hero-bg.png')"
+      },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
