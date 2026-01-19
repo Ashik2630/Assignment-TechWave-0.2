a6669915b224da0551dfee9c96c41a3ad43bbe40
Ashik2630 <ashik426370@gmail.com>
2026-01-20 00:23:08 +0600

    complet TechWave section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        bannerImage:"url('./assets/hero-bg.png')"
+        bannerImage:"url('./assets/hero-bg.png')",
+        footerImage:"url('./assets/footer-bg.png')"
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
